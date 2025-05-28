820584c10c19b3934376275863a9eeab26a57b62
Fergrenteski <luizgrenfer@gmail.com>
2025-05-28 19:43:45 -0300

    Feature:
     - Adicionado Model Locais;
     - Adicionado LocalView;
     - Adicionado LocalService;
     - Adicionado LocalRepository;
     - Adicionado Modal para confirmar default.

---
+++ src/main/java/com/pucpr/anotaai/model/Local.java
@@ -0,0 +1,75 @@
+package com.pucpr.anotaai.model;
+
+import java.io.Serializable;
+
+public class Local implements Serializable {
+    private int id;
+    private String nome;
+    private String endereco;
+    private String cidade;
+    private String estado;
+    private Integer cep;
+    private Integer telefone;
+
+    public Local(int id, String nome, String endereco, String cidade, String estado, Integer cep, Integer telefone) {
+        this.id = id;
+        this.nome = nome;
+        this.endereco = endereco;
+        this.cidade = cidade;
+        this.estado = estado;
+        this.cep = cep;
+        this.telefone = telefone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public Integer getCep() {
+        return cep;
+    }
+
+    public void setCep(Integer cep) {
+        this.cep = cep;
+    }
+
+    public Integer getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(Integer telefone) {
+        this.telefone = telefone;
+    }
+}
